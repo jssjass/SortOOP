ccb894b6a0fbd3b5da158809389cfd63b965a791
A <aleksejjss66528@gmail.com>
2024-09-30 16:23:53 +0700

    Итерация 1: Создание заглушек для методов

---
+++ SortOOP.py
@@ -0,0 +1,29 @@
+class TextProcessor:
+    def __init__(self):
+        pass
+
+    def input_data(self):
+        pass
+
+    def generate_random_text(self):
+        pass
+
+    def extract_words(self, text):
+        pass
+
+    def sort_words_by_length(self):
+        pass
+
+    def display_results(self):
+        pass
+
+class ConsoleApp:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+if __name__ == "__main__":
+    app = ConsoleApp()
+    app.run()
